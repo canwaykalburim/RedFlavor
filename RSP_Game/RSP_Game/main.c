1ad6f9489f4cc93157605a8f5a60af124e51c052
canwaykalburim <canwaykalburim@gmail.com>
2018-07-31 11:35:21 +0900

    RockPaperScissors

--- RSP_Game/RSP_Game/main.c
+++ RSP_Game/RSP_Game/main.c
@@ -1,19 +1,22 @@
 #include "common.h"
 
-#define Options_Screen "C:\\Users\\DSM2018\\source\\repos\\RSP_Game\\Music\\Options_Screen.wav"
+#define Options_Screen "C:\\Users\\DSM2018\\Desktop\\소스 코드\\_C\\RSP_Game\\Music\\Options_Screen.wav"
+
+void Menu();
 
 void Muse()
 {
 	PlaySound(TEXT(Options_Screen), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 }
 
-int main() {
+int draw = 0, win = 0, lose = 0 ,cont = 0;
+
+void Game() {
 	int nInput, a;
-	int draw = 0, win = 0, cont = 0;
+	int WS = 0;
 
 	_getch();
-	_getch();
-
+	system("cls");
 	Muse();
 
 	while (1) {
@@ -62,6 +65,7 @@ int main() {
 				system("cls");
 				printf("\n   player: 가위\n   computer: 보\n\n   you win.\n");
 				win++;
+				WS++;
 				_getch();
 				_getch();
 				system("cls");
@@ -70,6 +74,7 @@ int main() {
 				system("cls");
 				printf("\n   player: 바위\n   computer: 가위\n\n   you win.\n");
 				win++;
+				WS++;
 				_getch();
 				_getch();
 				system("cls");
@@ -78,6 +83,7 @@ int main() {
 				system("cls");
 				printf("\n   player: 보자기\n   computer: 바위\n\n   you win.\n");
 				win++;
+				WS++;
 				_getch();
 				_getch();
 				system("cls");
@@ -85,23 +91,38 @@ int main() {
 			else if (nInput == 1 && a == 2) {
 				system("cls");
 				printf("\n   player: 가위\n   computer: 바위\n\n   you lose.\n");
+				lose++;
+				_getch();
+				_getch();
+				system("cls");
+				cont++;
 				break;
 			}
 			else if (nInput == 2 && a == 3) {
 				system("cls");
 				printf("\n   player: 바위, computer: 보\n\n   you lose.\n");
+				lose++;
+				_getch();
+				_getch();
+				system("cls");
+				cont++;
 				break;
 			}
 			else if (nInput == 3 && a == 1) {
 				system("cls");
 				printf("\n   player: 보자기\n   computer: 가위\n\n   you lose.\n");
+				lose++;
+				_getch();
+				_getch();
+				system("cls");
+				cont++;
 				break;
 			}
 		}
 		else {
 			system("cls");
 			printf("\n   다른 수를 입력해 주세요.\n");
-			win++;
+			cont--;
 			_getch();
 			_getch();
 			system("cls");
@@ -109,8 +130,82 @@ int main() {
 		cont++;
 		printf("\n   0을 입력하면 종료합니다.\n");
 	}
-	printf("\n\n   ");
-	printf("게임의 결과 : %d전 %d승 %d무\n", cont, win, draw);
+	printf("\n\n   게임의 결과 : %d전 %d승 %d무 %d패\n", cont, win, draw, lose);
 	_getch();
 	_getch();
+	system("cls");
+	Menu();
+}
+
+void Game_Ex() {
+	system("cls");
+	printf("\n   [게임 룰]\n");
+	printf("\n   1. 플레이어는 가위 바위 보 중 하나를 선택합니다.\n");
+	printf("\n   2. 선택하면 PC가 랜덤으로 가위 바위 보 중 하나를 선택합니다.\n");
+	printf("\n   3. 선택에 따라 승패가 갈리며 패배하면 메뉴로 돌아갑니다.\n");
+
+	printf("\n\n  메뉴로 돌아가기 ▼");
+	_getch();
+	_getch();
+	system("cls");
+	Menu();
+}
+
+void Menu() {
+	int nInput;
+
+	_getch();
+	Muse();
+	printf("\n   [가위 바위 보]\n");
+	printf("\n   1. 게임 시작\n\n   2. 게임 설명\n\n   3. 게임 종료\n");
+	while (1) {
+		printf("\n  ⇒ ");
+		scanf("%d", &nInput);
+
+		if (nInput == 1) {
+			Game();
+			break;
+		}
+		else if (nInput == 2) {
+			Game_Ex();
+			break;
+		}
+		else if (nInput == 3) {
+			printf("\n");
+			break;
+		}
+		else {
+			printf("\n   다시 입력해주세요.");
+		}
+	}
+}
+
+void Grapy() {
+	char Intro[22] = "재미있는 가위 바위 보";
+
+	_getch();
+
+	printf("\n   ");
+	for (int i = 0; i < 22; i++) {
+		printf("%c", Intro[i]);
+		Sleep(200);
+	}
+	printf("\n\n");
+	printf("   ..       ..   \t\t	       ..\n");
+	printf("    ..     ..   \t ........\t   ..  ..  ..\n");
+	printf("     ..   ..     \t..........\t..  .. ..  ..\n");
+	printf("     .......      \t..........\t .. .. ..  ..   ..\n");
+	printf("    .........   \t..........\t  .. .. .. ..  ..\n");
+	printf("    .........   \t ........\t  ..............\n");
+	printf("     .......   \t\t\t\t  .............\n");
+	printf("\t\t\t\t\t  ...........\n");
+	printf("\t\t\t\t\t   .........\n\n\n");
+	_getch();
+	_getch();
+	system("cls");
+}
+
+int main(void) {
+	Grapy();
+	Menu();
 }
