b5ec0b9da39054596c39956e4a5d3e551c5956b4
canwaykalburim <canwaykalburim@gmail.com>
2018-08-24 15:59:54 +0900

    WordCount

---
+++ WordCount/WordCount/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Baekjoon 1152¹ø ¹®Á¦
+
+int main(void) {
+	char nInput[1000002];
+	int count = 0;
+
+	while (scanf("%s", nInput) == 1) {
+		count++;
+	}
+	printf("%d", count);
+
+	return 0;
+}
